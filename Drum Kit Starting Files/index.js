b57ece173682a9a663d8cee2bceb996ab8372457
wisecrepin <ngomawisecrepin@gmail.com>
2026-02-05 06:58:54 -0500

    finishing the project with animations cool at it again

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,21 +2,21 @@ let audio = new Audio("./sounds/tom-1.mp3");
 
 function playSound(ele) {
   let crash;
-  if (ele.innerHTML == "w") {
+  if (ele.innerHTML == "w" || ele.key == "w") {
     crash = new Audio("sounds/crash.mp3");
-  } else if (ele.innerHTML == "a") {
+  } else if (ele.innerHTML == "a" || ele.key == "a") {
     crash = new Audio("sounds/kick-bass.mp3");
-  } else if (ele.innerHTML == "s") {
+  } else if (ele.innerHTML == "s" || ele.key == "s") {
     crash = new Audio("sounds/snare.mp3");
-  } else if (ele.innerHTML == "d") {
+  } else if (ele.innerHTML == "d" || ele.key == "d") {
     crash = new Audio("sounds/tom-1.mp3");
-  } else if (ele.innerHTML == "j") {
+  } else if (ele.innerHTML == "j" || ele.key == "j") {
     crash = new Audio("sounds/tom-2.mp3");
-  } else if (ele.innerHTML == "k") {
+  } else if (ele.innerHTML == "k" || ele.key == "k") {
     crash = new Audio("sounds/tom-3.mp3");
-  } else {
+  } else if (ele.innerHTML == "l" || ele.key == "l")
     crash = new Audio("sounds/tom-4.mp3");
-  }
+
   crash.play();
 }
 function changeView(i) {
@@ -25,5 +25,18 @@ function changeView(i) {
 for (let i = 0; i < document.querySelectorAll("button").length; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     playSound(this);
+    makeAnimation(this.innerHTML);
   });
+}
+document.addEventListener("keydown", function (event) {
+  playSound(event);
+  makeAnimation(event.key);
+});
+
+function makeAnimation(ele) {
+  let activeButton = document.querySelector("." + ele);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
